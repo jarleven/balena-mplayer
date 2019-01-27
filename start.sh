1117b9667b83df365894a7ba46f52ca248567d26
jarleven <jarleven@gmail.com>
2019-01-27 22:31:45 +0100

    Update start.sh
    Added loop and app parameter

--- start.sh
+++ start.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+export APP=${APP:=1}
 export URL=${URL:="rtsp://192.168.1.10:554/user=admin&password=&channel=1&stream=0.sdp?real_stream"}
 export CACHE=${CACHE:=2000}
 export VOLUME=${VOLUME:=100}
 
-mplayer -cache ${CACHE} \
+if [ $APP -eq "1" ]; then
+  while true
+  do
+   echo "Starting mplayer with url $URL"
+   sleep 10
+   mplayer -cache ${CACHE} \
         -quiet \
         -volume $VOLUME \
         $URL
+  done
+fi
+
  
 # Consider this... 
 # while true
